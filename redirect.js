3d92ddb4721f555409b54dfabc7449764c6039a7
karrarkhazaal <karrar.hussein@zaincash.iq>
2025-10-21 16:36:02 +0300

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,9 @@
+<script>
+if (/android/i.test(navigator.userAgent)) {
+    window.location.href = "https://play.google.com/store/apps/details?id=mobi.foo.zaincash";
+} else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    window.location.href = "https://apps.apple.com/us/app/zaincash-iraq-%D8%B2%D9%8A%D9%86-%D9%83%D8%A7%D8%B4-%D8%B9%D8%B1%D8%A7%D9%82/id1176557130";
+} else {
+    window.location.href = "https://zaincash.iq/ar";
+}
+</script>
